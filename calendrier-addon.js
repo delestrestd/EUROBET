883669bd5982825feb3cf51524247bb171ae4af3
delestrestd <132286650+delestrestd@users.noreply.github.com>
2026-09-18 13:12:54 +0200

    Calendriers v6c3: rename patch to .b64.partN for Pages + update loader

--- calendrier-addon.js
+++ calendrier-addon.js
@@ -21,8 +21,8 @@
     }));
     (0,eval)(atob(texts.join("")));
     const patches=await Promise.all([0,1,2,3,4,5,6,7].map(async i=>{
-      const r=await fetch("./calendrier-v6c3.b64."+i+"?v=6c3",{cache:"no-cache"});
-      if(!r.ok)throw new Error("patch b64 "+i);
+      const r=await fetch("./calendrier-v6c3.b64.part"+i+"?v=6c3",{cache:"no-cache"});
+      if(!r.ok)throw new Error("patch b64 part"+i);
       return (await r.text()).replace(/\s+/g,"");
     }));
     (0,eval)(atob(patches.join("")));
