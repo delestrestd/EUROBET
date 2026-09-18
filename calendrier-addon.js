24cae030fe4b92cd5542045761a991f40581b7ab
delestrestd <132286650+delestrestd@users.noreply.github.com>
2026-09-18 09:25:26 +0200

    Calendriers: loader v6c + VERSION (28 data parts)

--- calendrier-addon.js
+++ calendrier-addon.js
@@ -4,7 +4,7 @@
     const texts = await Promise.all([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(async (i) => {
       const r = await fetch("./calendrier-addon.b64.part" + i + "?v=6c", { cache: "no-cache" });
       if (!r.ok) throw new Error("b64 part" + i + " HTTP " + r.status);
-      return (await r.text()).trim();
+      return (await r.text()).replace(/\s+/g, "");
     }));
     (0, eval)(atob(texts.join("")));
   }
